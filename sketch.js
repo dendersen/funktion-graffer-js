a8aee81c5224f9c016360b68c02566eeafe621a9
dendersen <dendersen@endersen.dk>
2022-01-07 11:30:20 +0100

    made graf

--- sketch.js
+++ sketch.js
@@ -4,37 +4,70 @@ function setup() {
   windowHeight -= 10
   background(80)
   strokeWeight(LI-1)
-  line(windowWidth/2, windowHeight, windowWidth/2, 20)
-  line(windowWidth, windowHeight/2, 20, windowHeight/2)
-  //y=-windowHeight/2
-  //x=-windowWidth/2
+  line(width/2, height-(height/30), width/2, (height/30))
+  line(width-(width/40), height/2, (width/40), height/2)
+  y=-height/2
+  x=-width/2
+  frameRate(600)
 }
 
 var x = 0
-var y = 0
+var y = 30
 var LI = 3
 function draw() {
-  pointMove()
   lineDraw()
   pointDraw()
+  pointMove()
 }
 
 function pointDraw(){ //makes a point showing where is currently being drawn
   stroke(255)
   strokeWeight(LI+1)
-  point(x+windowWidth/2,-y+windowHeight/2)
-
+  point(x+width/2,-y+height/2)
+  
   //draws lines on x and y for drawn point
-  strokeWeight(LI+3) 
-  point(x+windowWidth/2,windowHeight/2)
-  point(windowWidth/2,-y+windowHeight/2)
+  stroke(colR, colG, colB)
+  strokeWeight(LI-2) 
+  point(x+width/2,height/2)
+  point(width/2,-y+height/2)
   strokeWeight(LI)
   stroke(0)
 }
 
 
-function pointMove(){
+let colR = 255
+let colG = 0
+let colB = 0
+let action = 0
 
+function funk(){
+  let z =x
+  y=Math.sin((z)/5)*70
+}
+
+function pointMove(y){
+  if(x<width/2){
+  x+=0.1
+  }else{
+    x=-height/2
+    if(action<256) {
+      colR -=32
+      colB +=32
+      action +=32
+    }else if(action<512){
+      colB -=32
+      colG +=32
+      action +=32
+    }else if(action<768){
+      colG -=32
+      colR +=32
+      action +=32
+    }else{
+      action=0
+      pointMove()
+    }
+  }
+  funk()
 }
 
 function lineDraw(){
