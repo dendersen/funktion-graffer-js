930c87dca8c86738585542edc6d475ceda9942f6
dendersen <dendersen@endersen.dk>
2022-01-17 10:59:30 +0100

    more fun

--- drawing modes/multy y.js
+++ drawing modes/multy y.js
@@ -13,12 +13,25 @@ function MultyFunk(Positive){
   let t
   let T
   z = x
-  //t=+-sqrt(-x^2+2*X*centerX+r^2-x^2) "runs as both positive and negative"
+
+  //next two lines are about how to draw a circle
+  //t=+-sqrt(-x^2+2*X*centerX+r^2) "runs as both positive and negative"
   //T=centerY "a constant not affected by positivie negative change"
   
-  t=Math.sqrt(-Math.pow(z,2)+2*z*100+20000)
+  t=Math.sqrt(-Math.pow(z,2)+2*z*100+100000^2/*-Math.pow(z,2)*/)
   T=100
   
   if(Positive) return T+t
   else return T-t
+}
+
+function multyFunk1(CC){
+  let t
+  let T
+  let z = x/1000
+
+  t=Math.sqrt(Math.sin(z)*10+Math.cos(z*z))*Math.tan(z+2)
+  T=5
+  if(CC) return T+t
+  else return t-T
 }
